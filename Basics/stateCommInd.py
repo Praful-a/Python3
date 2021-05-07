58df33d9001ea6cb0e99cee9a40ecb295d81469c
Praful <prafuldubey27@gmail.com>
2021-05-07 21:38:02 +0530

    Statement comments in python

---
+++ Basics/stateCommInd.py
@@ -0,0 +1,34 @@
+# Declared using continuation character (\):
+# s = 1 + 1 + 1 +\
+# 	1 + 1 + 1
+# print(s)
+
+# Declared using parenthases () :
+# n = (1 + 2 + 3 + 4 + 5)
+# print(n)
+
+# Declared using square brackets []:
+# footballer = ["Messi",
+# 			  "Neymar",
+# 			  "Suarez"]
+# print(footballer)
+
+# Declared using braces {} :
+# x = {1 + 2 + 3 + 4 + 5}
+# print(x)
+
+# Declared using semicolons(;) :
+# flag = 2; ropes = 3; pole = 4
+# print(flag)
+# print(ropes)
+# print(pole)
+
+# program showing indentation
+# site = "gfg"
+
+# if site == "gfg":
+# 	print("Logging on to geeksforgeeks...")
+# else:
+# 	print("retype the URL..")
+# print('All set !')
+
