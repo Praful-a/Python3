8a7eb4f5e7d5ebfaef6051eeead11b9ee505b585
Praful <prafuldubey27@gmail.com>
2021-05-08 19:21:10 +0530

    variables in python.

---
+++ Variables/var.py
@@ -0,0 +1,50 @@
+# Variables need not be delcared first in python. They 
+# can be used directly
+
+# a = 3
+# A = 4
+# print(a)
+# print(A)
+
+# Arithmetic operations
+# a = 2
+# b = 3
+# c = a + b
+# print(c)
+# d = a * b
+# print(d)
+
+# Conditions 
+
+# a = 3
+# b = 9
+# if b % a == 0:
+# 	print("b is divisible by a")
+# elif b + 1 == 10:
+# 	print("Increment in b produces 10")
+# else:
+# 	print("You are in else statement!")
+
+# functions
+
+# def oddEven(a):
+# 	if a % 2 == 0:
+# 		return True
+	
+# 	return False
+
+# if oddEven(3) == True:
+# 	print("Number is Even")
+# else:
+# 	print("Number is odd")
+
+# Maximum possible value of an integer in Python
+
+# large numbers in Python
+# no restriction for storing large values in python
+# x = 10000000000000000000000000000000000000000000
+# x = x + 1
+# print(x)
+
+
+
