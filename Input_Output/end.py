f7a581d4e84c2bb3686821740c697b793a98ecb1
Praful <prafuldubey27@gmail.com>
2021-05-08 18:43:22 +0530

    sep and end uses in print function

---
+++ Input_Output/end.py
@@ -0,0 +1,27 @@
+# using of end in print function
+
+# ends the output with a <space>
+# print("Welcome to", end=" ")
+# print("India", end=" ")
+
+
+# ends the output with '@'
+# print("Python", end="@")
+# print("is most popular programming language!")
+
+# code for disabling the softspace feature
+# print('G', 'F', 'G', sep='')
+
+# # for formatting a date
+# print('09', '12', '2016', sep='-')
+
+# # another example
+# print('pratik', 'geeksforgeeks', sep='@')
+
+print('G', 'F', sep='', end='')
+print('G')
+#\n provides new line after printing the year
+print('09', '12', '2016', sep='-', end='\n')
+
+print('The', 'rock', sep='', end='@')
+print('geeksforgeeks')
