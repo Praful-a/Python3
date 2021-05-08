4b124ffefe88a755a1131e7fd61554b356f6d810
Praful <prafuldubey27@gmail.com>
2021-05-08 22:11:19 +0530

    encoding and decoding

---
+++ Variables/encoding_decoding.py
@@ -0,0 +1,28 @@
+# Code to deomnstrate String encoding
+
+# a = "GeeksforGeeks"
+
+# c = b'GeeksforGeeks'
+
+# # using encode() to encode the String
+# # encoded version of a is stored in d
+# # using ASCII mapping
+# d = a.encode('ASCII')
+# print(d)
+
+# if(d == c):
+# 	print("Encoding successful")
+# else:
+# 	print("Encoding Unseccessful")
+
+# Code to deomstrate Byte Decoding
+# a = 'GeeksforGeeks'
+# c = b'GeeksforGeeks'
+
+# d = c.decode('ASCII')
+# print(d)
+# print()
+# if (d == a):
+# 	print('Decoding successful')
+# else: print('Decoding Unseccessful')
+
