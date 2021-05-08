9db106b7ba9dcc05af0bea0508168f4ba72e74ee
Praful <prafuldubey27@gmail.com>
2021-05-08 22:20:12 +0530

    single and multiple variables and swapping

---
+++ Variables/single_multi_var.py
@@ -0,0 +1,30 @@
+# Equivalent codes in Python
+# (Produces same output)
+
+# print(1)
+
+# print((1))
+
+# Printing multiple values
+
+# print(1, 2)
+
+# print((1, 2))
+
+# print(1, end=" ")
+# print(2)
+
+# swaping with third variable
+# a = 10
+# b = 30
+
+# temp = a
+# a = b
+# b = temp
+# print(a)
+# print(b)
+
+# swaping wihtout using third var
+a, b = 10, 20
+a, b = b, a
+print(a, b)
