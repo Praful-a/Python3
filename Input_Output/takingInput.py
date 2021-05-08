e6575f87848b902d7c9fc68c695246b9cfbf7e35
Praful <prafuldubey27@gmail.com>
2021-05-08 17:18:18 +0530

    taking multiple inputs

--- Input_Output/takingInput.py
+++ Input_Output/takingInput.py
@@ -7,11 +7,25 @@
 # Program to check input
 # type in Python
 
-num = input ("Enter number : ")
-print(num)
-name1 = input("Enter name : ")
-print(name1)
+# num = input ("Enter number : ")
+# print(num)
+# name1 = input("Enter name : ")
+# print(name1)
 
 # Printing type of input value
-print("type of number", type(num))
-print("type of name", type(name1))
+# print("type of number", type(num))
+# print("type of name", type(name1))
+
+# input
+# num1 = int(input())
+# num2 = int(input())
+
+# printing the sum in integer
+# print(num1 + num2)
+
+# input
+num1 = float(input())
+num2 = float(input())
+
+# printing the sum in float
+print(num1 + num2)
