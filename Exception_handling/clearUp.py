f380c2275968b35f5eaa820b74529b34eedccef4
rock <prafuldubey27@gmail.com>
2021-05-15 10:32:27 +0530

    completed exception handling

---
+++ Exception_handling/clearUp.py
@@ -0,0 +1,26 @@
+# Code to illustrate clean up actions
+# def divide(x, y):
+# 	try:
+# 		result = x // y
+# 	except ZeroDivisionError:
+# 		print("Sorry ! You are dividing by zero")
+# 	else:
+# 		print("Yeah ! Your answer is :", result)
+# 	finally:
+# 		print("I'm finally caluse, alsways raised !! ")
+
+# divide(3, 0)
+
+def divide(x, y):
+    try:
+        # Floor Division : Gives only Fractional Part as Answer
+        result = x // y
+    except ZeroDivisionError:
+        print("Sorry ! You are dividing by zero ")
+    else:
+        print("Yeah ! Your answer is :", result)
+    finally:
+        print("I'm finally clause, always raised !! ")
+  
+# Look at parameters and note the working of Program
+divide(3, "3")
