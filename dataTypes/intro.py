91c822ce10aea93ad0dd6886dcd22622a606a231
rock <prafuldubey27@gmail.com>
2021-05-16 12:29:44 +0530

    data type intro

---
+++ dataTypes/intro.py
@@ -0,0 +1,39 @@
+# Assigning string to a variable
+# a = "This is a string"
+# print(a)
+
+
+# Declaring a list
+# L = [1, "a", "string", 1+2]
+# print(L)
+# L.append(6)
+# print(L)
+# L.pop()
+# print(L)
+# print(L[1])
+
+# Tuple
+# tup = (1, "a", "string", 1+2)
+# print(tup)
+# print(tup[1])
+
+# Iteration 
+
+# i = 1
+# while(i < 10):
+# 	print(i)
+# 	i += 1
+
+# Iteration by for loop on string
+# s = "Hello World"
+# for i in s:
+# 	print(i)
+
+# Iteration by for loop on list
+# L = [1, 4, 5, 7, 9, 8]
+# for i in L:
+# 	print(i)
+
+# Iteration by for loop for range
+for i in range(0, 10):
+	print(i)
