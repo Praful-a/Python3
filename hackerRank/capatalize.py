afbd0fb74d95c4f9115e822cdfa4866cffa3dca8
rock <prafuldubey27@gmail.com>
2021-06-30 15:53:49 +0530

    modified one line.

--- hackerRank/capatalize.py
+++ hackerRank/capatalize.py
@@ -1,8 +1,7 @@
 def solve(s):
-	str = s.split()
-	return " ".join([i.capitalize() for i in str])
+	return " ".join([i.capitalize() for i in s.split()])
 
 if __name__ == '__main__':
-	s = "chris alan lol"
+	s = "q w e r t y u i o p a s d f g h j  k l z x c v b n m Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M"
 	result = solve(s)
 	print(result)
