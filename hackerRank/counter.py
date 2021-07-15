8e63117cca1c69025535e900fefa975c3bd1d960
Praful-a <prafuldubey27@gmail.com>
2021-07-15 09:53:39 +0530

    solve counter problem

--- hackerRank/counter.py
+++ hackerRank/counter.py
@@ -1,17 +1,16 @@
 from collections import Counter
-l = [2, 3, 4, 5, 6, 8, 6, 5, 18]
-counter = Counter(l)
-print(counter)
-c = 0
-s = 0	
-for _ in range(6):
-	n, p = input().split()
-	num = int(n)
-	price = int(p)
-	if num in counter:
-		c += 1
-		if (c != counter[num]):
-			s += price 
-		else:
-			s += 0		 
-print(s)
+if __name__ == '__main__':
+	x = int(input())
+	l = list(map(int, input().split()))
+	counter = Counter(l)
+	print(counter)
+	N = int(input())
+	s = 0	
+	for _ in range(N):
+		n, p = input().split()
+		num = int(n)
+		price = int(p)
+		if counter[num]:
+			counter[num] -= 1 
+			s = s + price
+	print(s)
