989d89b85020f9d79545d08631929d19efcb5b5a
Praful-a <prafuldubey27@gmail.com>
2021-07-25 13:16:37 +0530

    set methods problem

--- hackerRank/Set_methods.py
+++ hackerRank/Set_methods.py
@@ -1,13 +1,20 @@
-if __name__ == '__main__':
-	n = int(input())
-	s = set(map(int, input().split()))
-	N = int(input())
-	for _ in range(N):
-		name = input().split()
-		if name[0] == 'pop':
-			s.pop()
-		elif name[0] == 'remove':
-			s.remove(int(name[1]))
-		elif name[0] == 'discard':
-			s.discard(int(name[1]))
-	print(s)
+n = int(input())
+s = set(map(int, input().split()))
+terms = int(input())
+for _ in range(terms):
+    comm = input().split()
+    for i in range(1, len(comm)):
+        comm[i] = int(comm[i])
+    if comm[0] == 'pop':
+        s.pop()
+    if comm[0] == 'remove':
+        s.remove(int(comm[1]))
+    if comm[0] == 'discard':
+        s.discard(comm[1])
+sum = 0
+for i in s:
+    sum += i
+if i:
+    print(sum)
+else:
+    print(sum)
